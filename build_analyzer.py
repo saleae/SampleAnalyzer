ccb164e072a12a73500ed7e418dccdc67c108679
Christian Werling <crwerling@gmail.com>
2019-07-15 17:06:39 +0200

    Specify C++ standard in build command to fix compilation on macOS

--- build_analyzer.py
+++ build_analyzer.py
@@ -43,8 +43,8 @@ include_paths = [ "./AnalyzerSDK/include" ]
 link_paths = [ "./AnalyzerSDK/lib" ]
 link_dependencies = [ "-lAnalyzer" ] #refers to libAnalyzer.dylib or libAnalyzer.so
 
-debug_compile_flags = "-O0 -w -c -fpic -g"
-release_compile_flags = "-O3 -w -c -fpic"
+debug_compile_flags = "-std=c++11 -O0 -w -c -fpic -g"
+release_compile_flags = "-std=c++11 -O3 -w -c -fpic"
 
 def run_command(cmd):
     "Display cmd, then run it in a subshell, raise if there's an error"
